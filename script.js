2f25461eaa277655f3f9e8b42865e9ceada96d8c
pratikkawane44 <145637808+pratikkawane44@users.noreply.github.com>
2026-06-15 23:42:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,87 @@
+ 
+const startBtn =
+  document.getElementById("startBtn");
+
+const introScreen =
+  document.getElementById("introScreen");
+
+const mainScreen =
+  document.getElementById("mainScreen");
+
+const successScreen =
+  document.getElementById("successScreen");
+
+const yesBtn =
+  document.getElementById("yesBtn");
+
+const noBtn =
+  document.getElementById("noBtn");
+
+const noTexts = [
+  "No",
+  "Really?",
+  "Think again",
+  "Impossible",
+  "Try YES ❤️"
+];
+
+let index = 0;
+
+/* START WEBSITE */
+
+startBtn.addEventListener("click", () => {
+
+  introScreen.classList.add("hidden");
+
+  setTimeout(() => {
+
+    mainScreen.classList.remove("hidden");
+
+  }, 600);
+
+});
+
+/* RUNNING NO BUTTON */
+
+noBtn.addEventListener("mouseover", () => {
+
+  const x = Math.random() * 300 - 150;
+
+  const y = Math.random() * 220 - 110;
+
+  noBtn.style.transform =
+    `translate(${x}px, ${y}px)`;
+
+  if(index < noTexts.length - 1){
+
+    index++;
+
+    noBtn.innerText =
+      noTexts[index];
+
+  }
+
+});
+
+/* YES CLICK */
+
+yesBtn.addEventListener("click", () => {
+
+  mainScreen.classList.add("hidden");
+
+  setTimeout(() => {
+
+    successScreen.classList.remove("hidden");
+
+  }, 700);
+
+  confetti({
+    particleCount:250,
+    spread:120,
+    origin:{
+      y:0.6
+    }
+  });
+
+});
+```
